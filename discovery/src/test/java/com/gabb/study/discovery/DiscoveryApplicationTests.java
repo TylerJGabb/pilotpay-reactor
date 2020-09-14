195bdcbd10e3c35311dd4bf533b441229f75ba58
Tyler Gabb <tylerjgabb@gmail.com>
2020-09-14 09:38:06 -0700

    added discovery

---
+++ discovery/src/test/java/com/gabb/study/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabb.study.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
