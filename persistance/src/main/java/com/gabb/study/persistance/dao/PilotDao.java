18f9d0fc25281339bed9bd2dcb4e05750c2b9362
Tyler Gabb <tylerjgabb@gmail.com>
2020-09-14 21:22:32 -0700

    fixing inconsistency between repository layer and api layer

--- persistance/src/main/java/com/gabb/study/persistance/dao/PilotDao.java
+++ persistance/src/main/java/com/gabb/study/persistance/dao/PilotDao.java
@@ -3,8 +3,10 @@ package com.gabb.study.persistance.dao;
 import com.gabb.study.persistance.entities.Pilot;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.UUID;
+
 /**
  * Automatically comes out of the box with basic rest endpoints
  * for obtaining data
  */
-public interface PilotDao extends CrudRepository<Pilot, Long> {}
+public interface PilotDao extends CrudRepository<Pilot, UUID> {}
